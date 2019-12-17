f21632c14f47794c65d8e3fb38af759230fb9796
Kutman Mairambek uulu <muctep.k@gmail.com>
2019-12-17 14:56:02 +0600

    Добавлен еще один товар, добавлен "slug", появилось понятие "скидка", "новый товар"

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.utils.text import slugify
-
+from datetime import date
 
 class Product(models.Model):
     name = models.CharField(max_length=512, verbose_name='Название')
@@ -11,16 +11,24 @@ class Product(models.Model):
     description = RichTextUploadingField('content')
     in_stock = models.BooleanField(verbose_name='В наличии', default=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
+    old_price = models.IntegerField(verbose_name='Старая цена', null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата добавления', null=True)
 
     def save(self, *args, **kwargs):
         self.slug = self.slug or slugify(self.name)
         super().save(*args, **kwargs)
 
+    def get_discount(self):
+        return round((self.old_price-self.price)/((self.price+self.old_price)/2)*100)
+
     def main_image(self):
         return self.images.all().first().image
 
     def get_images(self):
-        return [item.image for item in self.images.all()]
+        return [item.image.url for item in self.images.all()]
+
+    def is_new(self):
+        return (date.today() - date(self.created_at)).days <= 7
 
     def __str__(self):
         return self.name
